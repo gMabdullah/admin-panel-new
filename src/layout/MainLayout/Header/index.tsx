84411ef2429af42aaaa0ab37ee241c0666b1afde
tdusman <usman.sheikh@tossdown.com>
2022-09-02 11:02:12 +0500

    header component done

--- src/layout/MainLayout/Header/index.tsx
+++ src/layout/MainLayout/Header/index.tsx
@@ -61,19 +61,19 @@ const Header = () => {
             </Box>
 
             {/* header search */}
-            <SearchSection />
+            {/* <SearchSection /> */}
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ flexGrow: 1 }} />
 
             {/* mega-menu */}
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {/* <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                 <MegaMenuSection />
-            </Box>
+            </Box> */}
 
             {/* live customization & localization */}
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {/* <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                 <LocalizationSection />
-            </Box>
+            </Box> */}
 
             {/* notification & profile */}
             <NotificationSection />
