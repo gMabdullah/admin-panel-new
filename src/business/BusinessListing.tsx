eaf84664a6c10d9ff0e4c8d3163d355f2ebd288b
tdusman <usman.sheikh@tossdown.com>
2022-12-25 15:36:42 +0500

    pr feedback fixed

--- src/business/BusinessListing.tsx
+++ src/business/BusinessListing.tsx
@@ -15,6 +15,18 @@ import CustomButton from "components/CustomButton";
 import TdTextField from "components/TdTextField";
 import { clearStorage } from "orders/HelperFunctions";
 
+import {
+  contentStyle,
+  mainCardStyle,
+  grid12Style,
+  grid11Style,
+  searchFieldStyle,
+  grid1Style,
+  businessListingBoxStyle,
+  businessListingCardStyle,
+  cardMediaStyle,
+} from "./Styles";
+
 const BusinessListing = () => {
   let timeOut: NodeJS.Timeout;
   const navigate = useNavigate();
@@ -33,7 +45,8 @@ const BusinessListing = () => {
   };
 
   const handleSearchChange = (e: { target: { value: string } }) => {
-    clearTimeout(timeOut); // to stop setting Timeout on typing of every single character
+    // to stop setting Timeout on typing of every single character
+    clearTimeout(timeOut);
 
     timeOut = setTimeout(() => {
       if (e.target.value) {
@@ -54,72 +67,23 @@ const BusinessListing = () => {
     <MainCard
       dividerSX={{ m: "unset", display: "none" }}
       headerSX={{ p: "unset !important", mb: "30px" }}
-      contentSX={{
-        "& .MuiCardContent-root": {
-          p: "unset !important",
-        },
-        m: "unset",
-        p: "unset !important",
-      }}
-      sx={{
-        m: "unset",
-        borderRadius: "unset",
-        p: "32px",
-        border: "none",
-      }}
+      contentSX={contentStyle}
+      sx={mainCardStyle}
       title={
         <Grid container>
-          <Grid
-            item
-            xs={12}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Grid
-              item
-              xs={11}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
+          <Grid item xs={12} sx={grid12Style}>
+            <Grid item xs={11} sx={grid11Style}>
               <Typography variant="h2">Businesses</Typography>
 
               <TdTextField
                 type="search"
                 placeholder="Search Profile"
                 onChange={handleSearchChange}
-                sx={{
-                  width: "260px",
-                  height: "44px",
-                  marginLeft: "24px",
-
-                  "& .MuiOutlinedInput-input": {
-                    background: "#F5F5F5",
-                    padding: "10px 16px 10px 0px !important",
-                  },
-                  "& .MuiOutlinedInput-notchedOutline": {
-                    border: "unset",
-                  },
-                  "& .MuiOutlinedInput-root": {
-                    backgroundColor: "#F5F5F5",
-                  },
-                }}
+                sx={searchFieldStyle}
               />
             </Grid>
 
-            <Grid
-              item
-              xs={1}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "end",
-              }}
-            >
+            <Grid item xs={1} sx={grid1Style}>
               <CustomButton
                 variant={"contained"}
                 color={"secondary"}
@@ -135,13 +99,7 @@ const BusinessListing = () => {
         </Grid>
       }
     >
-      <Box
-        sx={{
-          borderRadius: "8px",
-          p: "24px 30px",
-          background: "#E5E7EB",
-        }}
-      >
+      <Box sx={businessListingBoxStyle}>
         <Grid container spacing={{ xs: 1 }} columns={{ xs: 4 }}>
           {businesses &&
             businesses.map((business: BusinessInfo, cardIndex: number) => (
@@ -153,13 +111,7 @@ const BusinessListing = () => {
                   p: "8px 0px 0px  8px !important",
                 }}
               >
-                <Card
-                  sx={{
-                    background: "#FFFFFF",
-                    border: "1px solid rgba(0, 0, 0, 0.16)",
-                    borderRadius: "8px",
-                  }}
-                >
+                <Card sx={businessListingCardStyle}>
                   <CardActionArea
                     sx={{ p: "8px" }}
                     onClick={() => handleBusinessSelection(cardIndex)}
@@ -173,15 +125,7 @@ const BusinessListing = () => {
                         component="img"
                         image={business.eatout_logo}
                         alt="Logo"
-                        sx={{
-                          height: "56px",
-                          width: "56px",
-                          borderRadius: "8px",
-                          mr: "12px",
-                          p: "3px",
-                          border: "1px solid rgba(0, 0, 0, 0.12)",
-                          objectFit: "contain",
-                        }}
+                        sx={cardMediaStyle}
                       />
 
                       <Typography
