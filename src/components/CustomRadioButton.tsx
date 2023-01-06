c0e06219c6aace4a71d1324ef8b665d6469350cc
tdusman <usman.sheikh@tossdown.com>
2023-01-06 19:51:02 +0500

    radio button section done

--- src/components/CustomRadioButton.tsx
+++ src/components/CustomRadioButton.tsx
@@ -14,18 +14,20 @@ type radioButtonTypes<T extends React.ElementType> = {
   label?: string;
   options?: { value: string; label: string }[];
   sx?: Object;
+  row?: boolean;
 } & React.ComponentPropsWithoutRef<T>;
 
 const CustomRadioButton = <T extends React.ElementType = "select">({
   onChange,
   value,
   options,
+  row,
   ...rest
 }: radioButtonTypes<T>): JSX.Element => {
   return (
     <>
       <FormControl>
-        <RadioGroup onChange={onChange} value={value} {...rest}>
+        <RadioGroup row={row} onChange={onChange} value={value} {...rest}>
           {options?.map<object>((option, index) => (
             <FormControlLabel
               value={option.value}
--- src/views/dashboard/orders/DashboardOrders.tsx
+++ src/views/dashboard/orders/DashboardOrders.tsx
@@ -1,8 +1,44 @@
+import { Stack, Typography } from "@mui/material";
+import CustomRadioButton from "components/CustomRadioButton";
+import { useState } from "react";
+
 const DashboardOrders = () => {
+  const [deliveryChargesList, setDeliveryChargesList] = useState([
+    {
+      label: "",
+      value: "",
+    },
+  ]);
+
   return (
-    <div>
-      <h1>Dashboard Orders Component</h1>
-    </div>
+    <Stack>
+      <Typography variant="h5" sx={{ mb: "12px" }}>
+        Display
+      </Typography>
+
+      <CustomRadioButton
+        row={true}
+        sx={{
+          "& .MuiFormControlLabel-root": {
+            mr: "25px",
+          },
+          "& .MuiTypography-root": {
+            fontSize: "12px",
+          },
+          "& .MuiRadio-root ": {
+            p: "8px",
+          },
+        }}
+        options={[
+          { label: "All", value: "0" },
+          { label: "None", value: "1" },
+          { label: "Web", value: "2" },
+          { label: "POS", value: "3" },
+        ]}
+        value={""} // onChange={canadaPostHandler}
+        // value={canadaPostDeliveryCharges}
+      />
+    </Stack>
   );
 };
 
