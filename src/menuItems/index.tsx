aa7941e668199ef17640e7f502b38fd784d3b9df
tdusman <usman.sheikh@tossdown.com>
2023-01-09 11:14:12 +0500

    api

--- src/menuItems/index.tsx
+++ src/menuItems/index.tsx
@@ -9,7 +9,11 @@ import Integrations from "./Integrations";
 import Settings from "./Settings";
 import { NavItemType } from "types";
 
+import useAxios from "axios-hooks";
+
 // ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = () => {};
 
 const MenuItems: { items: NavItemType[] } = {
   items: [
