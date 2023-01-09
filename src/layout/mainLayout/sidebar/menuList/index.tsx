6fb651ad31ea293e45b247c8981e8e79f5ecfaf0
tdusman <usman.sheikh@tossdown.com>
2023-01-09 13:47:18 +0500

    menu JSON api

--- src/layout/mainLayout/sidebar/menuList/index.tsx
+++ src/layout/mainLayout/sidebar/menuList/index.tsx
@@ -1,30 +1,64 @@
-import { memo } from "react";
+import React, { useEffect, memo, useState } from "react";
 
 // material-ui
 import { Typography } from "@mui/material";
 
+import useAxios from "axios-hooks";
+
 // project imports
-import menuItem from "menuItems";
+// import menuItem from "menuItems";
 import NavCollapse from "./navCollapse";
 import NavItem from "./navItem";
 
 // ==============================|| SIDEBAR MENU LIST ||============================== //
 
 const MenuList = () => {
-  const navItems = menuItem.items.map((item) => {
-    switch (item.type) {
-      case "collapse":
-        return <NavCollapse key={item.id} menu={item} level={1} />;
-      case "item":
-        return <NavItem key={item.id} item={item} level={1} />;
-      default:
-        return (
-          <Typography key={item.id} variant="h6" color="error" align="center">
-            Menu Items Error
-          </Typography>
-        );
+  const { eatout_id } = JSON.parse(localStorage.getItem("businessInfo")!);
+
+  const [menuItems, setMenuItems] = useState([]);
+
+  const menuItemsPayload = () => {
+    const formData = new FormData();
+
+    formData.append("business_id", eatout_id);
+    // formData.append("admin_id", user_id);
+    formData.append("source", "biz");
+    return formData;
+  };
+
+  const [{ data: menuItem }] = useAxios(
+    {
+      url: "/get_menu_json",
+      method: "POST",
+      data: menuItemsPayload(),
     }
-  });
+    // { manual: true }
+  );
+
+  useEffect(() => {
+    menuItem && menuItem.data && setMenuItems(menuItem.data.items);
+  }, [menuItem]);
+
+  let navItems: any[] = [];
+
+  if (menuItems.length > 0) {
+    console.log("mmmmmmmmmm = ", menuItems);
+
+    navItems = menuItems.map((item: any) => {
+      switch (item.type) {
+        case "collapse":
+          return <NavCollapse key={item.id} menu={item} level={1} />;
+        case "item":
+          return <NavItem key={item.id} item={item} level={1} />;
+        default:
+          return (
+            <Typography key={item.id} variant="h6" color="error" align="center">
+              Menu Items Error
+            </Typography>
+          );
+      }
+    });
+  }
 
   return <>{navItems}</>;
 };
--- src/menuItems/index.tsx
+++ src/menuItems/index.tsx
@@ -9,11 +9,7 @@ import Integrations from "./Integrations";
 import Settings from "./Settings";
 import { NavItemType } from "types";
 
-import useAxios from "axios-hooks";
-
 // ==============================|| MENU ITEMS ||============================== //
-
-const menuItems = () => {};
 
 const MenuItems: { items: NavItemType[] } = {
   items: [
