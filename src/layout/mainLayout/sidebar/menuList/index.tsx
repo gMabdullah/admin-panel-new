4592aa857cb8e47a3f1453cfe8e8e17ea3034541
tdusman <usman.sheikh@tossdown.com>
2023-01-09 18:18:46 +0500

    menu JSON api

--- src/layout/mainLayout/sidebar/menuList/index.tsx
+++ src/layout/mainLayout/sidebar/menuList/index.tsx
@@ -1,76 +1,73 @@
-import React, { useEffect, memo, useState } from "react";
+import { useEffect, useState } from "react";
 
 // material-ui
 import { Typography } from "@mui/material";
 
 import useAxios from "axios-hooks";
 
-// project imports
-// import menuItem from "menuItems";
+import Loader from "components/Loader";
 import NavCollapse from "./navCollapse";
 import NavItem from "./navItem";
-import Loader from "components/Loader";
 
 // ==============================|| SIDEBAR MENU LIST ||============================== //
 
 const MenuList = () => {
   const { eatout_id } = JSON.parse(localStorage.getItem("businessInfo")!);
 
-  const [menuItems, setMenuItems] = useState([]);
+  const [menuItems, setMenuItems] = useState<Element[] | []>([]);
 
-  const menuItemsPayload = () => {
+  const menuItemsAPIPayload = () => {
     const formData = new FormData();
 
     formData.append("business_id", eatout_id);
-    // formData.append("admin_id", user_id);
     formData.append("source", "biz");
     return formData;
   };
 
-  const [{ data: menuItem, loading: sideMenuLoader }, menuJSONAPICall] =
-    useAxios(
-      {
-        url: "/get_menu_json",
-        method: "POST",
-        data: menuItemsPayload(),
-      },
-      { manual: true }
-    );
+  const [{ loading: sideBarLoader }, menuItemsAPICall] = useAxios(
+    {
+      url: "/get_menu_json",
+      method: "POST",
+      data: menuItemsAPIPayload(),
+    },
+    { manual: true }
+  );
 
   useEffect(() => {
     (async () => {
-      await menuJSONAPICall();
+      const {
+        data: { data },
+      } = await menuItemsAPICall();
+
+      if (!Array.isArray(data)) {
+        const navItems = data.items.map((item: any) => {
+          switch (item.type) {
+            case "collapse":
+              return <NavCollapse key={item.id} menu={item} level={1} />;
+            case "item":
+              return <NavItem key={item.id} item={item} level={1} />;
+            default:
+              return (
+                <Typography
+                  key={item.id}
+                  variant="h6"
+                  color="error"
+                  align="center"
+                >
+                  Menu Items Error
+                </Typography>
+              );
+          }
+        });
+
+        setMenuItems(navItems);
+      }
     })();
   }, []);
 
-  useEffect(() => {
-    menuItem && menuItem.data && setMenuItems(menuItem.data.items);
-  }, [menuItem]);
-
-  let navItems: any[] = [];
-
-  if (menuItems.length > 0) {
-    navItems = menuItems.map((item: any) => {
-      switch (item.type) {
-        case "collapse":
-          return <NavCollapse key={item.id} menu={item} level={1} />;
-        case "item":
-          return <NavItem key={item.id} item={item} level={1} />;
-        default:
-          return (
-            <Typography key={item.id} variant="h6" color="error" align="center">
-              Menu Items Error
-            </Typography>
-          );
-      }
-    });
-
-    console.log("mmmmmmmmmm = ", navItems);
-  }
-
   return (
     <>
-      {sideMenuLoader && <Loader />} {navItems}
+      {sideBarLoader && <Loader />} {menuItems}
     </>
   );
 };
